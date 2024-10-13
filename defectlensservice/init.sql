8910e898ac23dd6e81b78fd71983c26beefa3a93
mrGnegich <97912575+GnegDev@users.noreply.github.com>
2024-10-13 09:49:26 +0300

    Update init.sql

--- defectlensservice/init.sql
+++ defectlensservice/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE laptop_record (
     id SERIAL PRIMARY KEY,
     cover_photo bytea,
-	screen_photo bytea,
+    screen_photo bytea,
     keyboard_photo bytea,
-    base_photo bytea
+    base_photo bytea,
+    serial TEXT
   );
