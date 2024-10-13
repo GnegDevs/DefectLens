2834b8aef93432b8b6af5aa4a1358c470725de95
mrGnegich <97912575+GnegDev@users.noreply.github.com>
2024-10-13 04:02:34 +0300

    Add files via upload

---
+++ defectlensinterface/defectlens/index.php
@@ -0,0 +1,127 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $url = 'http://localhost:8080/defectlens/api/v1/upload/images';
+    $data = [];
+
+    if (isset($_FILES['cover_photo']) && $_FILES['cover_photo']['error'] == UPLOAD_ERR_OK) {
+        $data['cover_photo'] = new CURLFile($_FILES['cover_photo']['tmp_name'], $_FILES['cover_photo']['type'], $_FILES['cover_photo']['name']);
+    }
+    if (isset($_FILES['screen_photo']) && $_FILES['screen_photo']['error'] == UPLOAD_ERR_OK) {
+        $data['screen_photo'] = new CURLFile($_FILES['screen_photo']['tmp_name'], $_FILES['screen_photo']['type'], $_FILES['screen_photo']['name']);
+    }
+    if (isset($_FILES['keyboard_photo']) && $_FILES['keyboard_photo']['error'] == UPLOAD_ERR_OK) {
+        $data['keyboard_photo'] = new CURLFile($_FILES['keyboard_photo']['tmp_name'], $_FILES['keyboard_photo']['type'], $_FILES['keyboard_photo']['name']);
+    }
+    if (isset($_FILES['base_photo']) && $_FILES['base_photo']['error'] == UPLOAD_ERR_OK) {
+        $data['base_photo'] = new CURLFile($_FILES['base_photo']['tmp_name'], $_FILES['base_photo']['type'], $_FILES['base_photo']['name']);
+    }
+    if (isset($_POST['serial'])) {
+        $data['serial'] = $_POST['serial'];
+    }
+
+    if (!empty($data)) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        if (is_numeric($response)) {
+            $id = intval($response);
+        } else {
+            echo "Ошибка: неверный формат ответа.";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload Images</title>
+    <style>
+        * {
+            font-family: sans-serif;
+        }
+        .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 10px;
+            max-width: 400px;
+            margin: auto;
+        }
+        .id-container {
+            display: grid;
+            gap: 10px;
+            max-width: 400px;
+            margin: 10px auto;
+        }
+        .id-field {
+            grid-column: span 2;
+        }
+        .image-box {
+            padding: 5px;
+            min-height: 16vh;
+            border: 2px solid black;
+            border-radius: 4px;
+        }
+        .container label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        .container input[type="file"] {
+            width: 100%;
+        }
+        button {
+            grid-column: span 2;
+            padding: 10px;
+            background-color: black;
+            color: white;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+
+        }
+        button:hover {
+            background-color: rgb(64, 64, 64);
+        }
+    </style>
+</head>
+<body>
+    <form action="index.php" method="post" enctype="multipart/form-data" class="container">
+        <div class="image-box">
+            <label for="cover_photo">Cover Photo:</label>
+            <input type="file" name="cover_photo" id="cover_photo"><br>
+        </div>
+        <div class="image-box">
+            <label for="screen_photo">Screen Photo:</label>
+            <input type="file" name="screen_photo" id="screen_photo"><br>
+        </div>
+        <div class="image-box">
+            <label for="keyboard_photo">Keyboard Photo:</label>
+            <input type="file" name="keyboard_photo" id="keyboard_photo"><br>
+        </div>
+        <div class="image-box">
+            <label for="base_photo">Base Photo:</label>
+            <input type="file" name="base_photo" id="base_photo"><br>
+        </div>
+        <input name="serial" placeholder="Serial number" class="id-field">
+        <button type="submit">Upload</button>
+    </form>
+
+    <form action="record.php" method="post" class="id-container">
+        <?php if (isset($id)): ?>
+            <p style="font-size: 16px">Your laptop record ID, click the button to scan the laptop:</p>
+            <input name="id" value="<?php echo $id; ?>" class="id-field">
+        <?php else: ?>
+            <p style="font-size: 16px">or</p>
+            <input name="id" placeholder="Record ID" class="id-field">
+        <?php endif; ?>
+            <button type="submit">Scan laptop by record ID</button>
+    </form>
+</body>
+</html>
---
+++ defectlensinterface/defectlens/record.php
@@ -0,0 +1,117 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $url = "http://localhost:8080/defectlens/api/v1/record/$id";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Выводим ответ сервера для отладки
+
+    $data = json_decode($response, true);
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo "Ошибка декодирования JSON: " . json_last_error_msg();
+        exit;
+    }
+
+    // Преобразуем строки в массивы байтов
+    $data['coverPhoto'] = base64_decode($data['coverPhoto']);
+    $data['screenPhoto'] = base64_decode($data['screenPhoto']);
+    $data['keyboardPhoto'] = base64_decode($data['keyboardPhoto']);
+    $data['basePhoto'] = base64_decode($data['basePhoto']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Record Details</title>
+    <style>
+        * {
+            font-family: sans-serif;
+        }
+        .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 10px;
+            max-width: 400px;
+            margin: auto;
+        }
+        .id-field {
+            grid-column: span 2;
+        }
+        .image-box {
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+            padding: 5px;
+            max-height: 50vh;
+            max-width: 50vh;
+            border: 2px solid lightgrey;
+            border-radius: 4px;
+        }
+        .image-box img {
+            object-fit: cover;
+            max-width: 50%;
+            max-height: 50%;
+        }
+        button {
+            grid-column: span 2;
+            padding: 10px;
+            background-color: black;
+            color: white;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+
+        }
+        button:hover {
+            background-color: rgb(64, 64, 64);
+        }
+        .button-container {
+            margin-top: 10px;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 10px;
+            max-width: 400px;
+            margin: 10px auto;
+        }
+    </style>
+</head>
+<body>
+    <?php if (isset($data)): ?>
+        <div class="container">
+        <h2 class="id-field">Record ID: <?php echo htmlspecialchars($data['id']); ?></h2>
+            <div class="image-box">
+                <h3>Cover Photo:</h3>
+                <img src="data:image/png;base64,<?php echo base64_encode($data['coverPhoto']); ?>" alt="Cover Photo">
+                <p style="font-size: 16px">Царапина</p>
+            </div>
+            <div class="image-box">
+                <h3>Screen Photo:</h3>
+                <img src="data:image/png;base64,<?php echo base64_encode($data['screenPhoto']); ?>" alt="Screen Photo">
+                <p style="font-size: 16px">Дефектов не обнаружено</p>
+            </div>
+            <div class="image-box">
+                <h3>Keyboard Photo:</h3>
+                <img src="data:image/png;base64,<?php echo base64_encode($data['keyboardPhoto']); ?>" alt="Keyboard Photo">
+                <p style="font-size: 16px">Проблемы с клавишами</p>
+            </div>
+            <div class="image-box">
+                <h3>Base Photo:</h3>
+                <img src="data:image/png;base64,<?php echo base64_encode($data['basePhoto']); ?>" alt="Base Photo">
+                <p style="font-size: 16px">Царапина</p>
+            </div>
+            <input readonly value="<?php echo $data['serial']?>" name="serial" placeholder="Serial number" class="id-field">
+        </div>
+        <form action="http://localhost:5000/defectlensmodel/api/v1/process/download" method="get" class="button-container">
+            <button type="submit">Get report</button>
+        </form>
+    <?php endif; ?>
+</body>
+</html>
